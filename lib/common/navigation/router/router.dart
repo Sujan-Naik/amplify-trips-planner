510c5e878abac68d9e3a6265341fdc04e6cd084e
Sujan Naik <sujannaik047@gmail.com>
2025-06-23 12:45:49 +0100

    Add the PastTrips details page to the app

--- lib/common/navigation/router/router.dart
+++ lib/common/navigation/router/router.dart
@@ -1,5 +1,6 @@
 import 'package:amplify_trips_planner/common/navigation/router/routes.dart';
 import 'package:amplify_trips_planner/features/trip/ui/edit_trip_page/edit_trip_page.dart';
+import 'package:amplify_trips_planner/features/trip/ui/past_trip_page/past_trip_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/past_trips/past_trips_list.dart';
 import 'package:amplify_trips_planner/features/trip/ui/trip_page/trip_page.dart';
 import 'package:amplify_trips_planner/features/trip/ui/trips_list/trips_list_page.dart';
@@ -29,6 +30,14 @@ final router = GoRouter(
         return EditTripPage(
           trip: state.extra! as Trip,
         );
+      },
+    ),
+    GoRoute(
+      path: '/pasttrip/:id',
+      name: AppRoute.pastTrip.name,
+      builder: (context, state) {
+        final tripId = state.pathParameters['id']!;
+        return PastTripPage(tripId: tripId);
       },
     ),
     GoRoute(
